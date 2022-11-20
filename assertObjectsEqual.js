2ec0500aa65cb38c8b3f3df0a0ec9190afed3dae
AndrewGalatsan <agalatsan@hotmail.com>
2022-11-20 22:15:33 +0000

    Updates

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -13,10 +13,15 @@ return false;
 };
 
 const assertObjectsEqual = function(actual,expected){
-  const check = require(`util`).check;
+  const inspect = require(`util`).inspect;
   if (actual !== expected) {
-  console.log(`Assertion Failed:  ${check(ab)} !== ${check(ba)}`);   
+  console.log(`Assertion Failed:  ${inspect(ab)} !== ${inspect(ba)}`);   
   } else {
-   console.log(`Assertion Passed:  ${check(ab)} === ${check(ba)}`); 
+   console.log(`Assertion Passed:  ${inspect(ab)} === ${inspect(ba)}`); 
 }
- }
+}
+
+const ab = { a: "4", b: "2"};
+const ba = { b: "2", a: "1"};
+console.log(assertObjectsEqual(eqObjects(ab,ba),false));
+
