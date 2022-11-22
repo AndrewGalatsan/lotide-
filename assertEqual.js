94d580d98878897eec9f71b5f682629bd918ab5d
AndrewGalatsan <agalatsan@hotmail.com>
2022-11-22 19:46:10 +0000

    Input notes on what the assertEqual function does and what the expected test output should be.

--- assertEqual.js
+++ assertEqual.js
@@ -7,6 +7,10 @@ if (actual === expected){
 }
 };
 
+// The function 'assertEqual' takes in two parameters of actual and expected and creates an 'if' condition to see if the actual and expected value are equal.
+// If equal, it console logs the string that says assertion as passed, noting the actual and expected value. Otherwise it console logs a failure.
+// THe below two tests should provide a 'fail' because they are not equal.
+
 // TEST CODE
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 2);
