a5d3604b000bea6232ced40180a3727d6169ad8a
AndrewGalatsan <agalatsan@hotmail.com>
2022-11-22 20:30:45 +0000

    Explained what the countOnly functino does and also console logged a test variable.

--- countOnly.js
+++ countOnly.js
@@ -26,6 +26,11 @@ for (const item of allItems) {
   return results;
 }
 
+// The countOnly function is similar to the countLetters function, however the main difference here is that we don't need the .replace method because
+// we're not looking to find exact matches of letters, but moreso exact counts of strings. This function takes in a parameter of allItems and itemsToCount,
+// and a new object is created named results. There is a for of loop ran that looks at each item in the input of allItems and writes a condition that 
+// says, if there is an identified indexed value under 'itemsToCount', take that indexed value and add 1 to it, within the results array. Otherwise
+// keep the value at 1. Then we return the new object 'results'.
 
 
 const firstNames = [
@@ -41,7 +46,7 @@ const firstNames = [
 ];
 
 const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
-
+console.log(result1)
 
 
 assertEqual(result1["Jason"], 1);
