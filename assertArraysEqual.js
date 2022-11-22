864765dff5224dfd5d0d95d643c7d6195b3c19a1
AndrewGalatsan <agalatsan@hotmail.com>
2022-11-22 16:55:09 +0000

    Provided explanation for what the eqArrays and assertArraysEqual functions does.

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -19,3 +19,12 @@ const assertArraysEqual = function(arr1,arr2){
         console.log(` YAY These arrays ARE equal`)
     }
 }
+
+// What the eqArrays function does is take in 2 parameters, array 1 and array 2. If the length of both arrays are not the same, a boolean value of
+// false gets returned. Furthermore, through a loop if the indexed value of array 1 and 2 are not the same, a boolean value of false is returned. 
+// For all other cases, a boolean value of true is to be returned.
+
+// What the assertArraysEquals function does is take in 2 parameters, array 1 and array 2. There is a named variable as 'result' which contains the
+// results of applying eqArrays functions to the new array 1 and 2. Through an if statement if the result is false; console.log a string noting
+// that these are not equal, otherwise console.log a string saying they are equal.
+
