ef6b85bc205e8341d7fc8cc967f83ac2d667f6ca
AndrewGalatsan <agalatsan@hotmail.com>
2022-11-22 20:18:12 +0000

    Explaining in detail what the countLetters function does and how it was structured.

--- countLetters.js
+++ countLetters.js
@@ -23,6 +23,12 @@ const assertEqual = function(actual, expected) {
     return final;
   };
   
+  // The countLetters function makes use of an arrow function and makes the parameter of 'obj' replace its input so that a new string is returned that 
+  // matches the /\s+/g, "" code I input. I created an empty object variable and then a for of loop so that for each index under obj(which has the .replace
+  // method applied to it), if there is a match that makes use of the .replace method, it takes that value and adds 1 to it, otherwise keeps it at 1.
+  // Then it returns the object that has this number count stored. The below is an output of how it should look like when applying the countLetters
+  // function to the Lighthouse string.
+
   assertEqual(countLetters("lighthouse in the house"), {
     l: 1,
     i: 2,
