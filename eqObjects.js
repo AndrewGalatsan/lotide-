5fdd81e72f8a081f3e50d55f2b1e2192b12aa6ac
AndrewGalatsan <agalatsan@hotmail.com>
2022-11-22 20:59:14 +0000

    Provided a detailed explanation of the eqObjects function and the output for the code.

--- eqObjects.js
+++ eqObjects.js
@@ -21,6 +21,12 @@ const assertArraysEqual = function(arr1,arr2){
     }
 }
 
+// What the eqObjects function does it takes in two parameters, object1 and object2. It then uses a built in method of Object.keys and applies it to
+// and if statement to see if the key of object1 and object2 are the same. If so, there is a let of loop ran that runs through object1 and uses a 
+// built in method to check if there is an array in the object of obect1 and object2. Then if the values in the array of object1 and object2 are not the same,
+// (symbolized by the !eqArrays function), a boolean of false is returned, otherwise it returns true.
+// The console logged test below should return true.
+
 const eqObjects = function(object1, object2){
   if (Object.keys(object1).length === Object.keys(object2).length){
     for(let i of Object.keys(object1)){
@@ -37,6 +43,6 @@ return false;
 
 const cd = { c: "1", d: ["2", 3] };
 const dc = { d: ["2", 3], c: "1" };
-eqObjects(cd, dc);
+console.log(eqObjects(cd, dc));
 
          
