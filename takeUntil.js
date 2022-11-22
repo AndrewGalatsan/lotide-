c17e1f226ccd37d93ad9bf47ec62437c439552fe
AndrewGalatsan <agalatsan@hotmail.com>
2022-11-22 00:02:32 +0000

    Callback function looking for truthy

---
+++ takeUntil.js
@@ -0,0 +1,49 @@
+const eqArrays = function(arr1,arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+
+const assertArraysEqual = function(arr1,arr2){
+  const result = eqArrays(arr1,arr2)
+    if (!result){
+        console.log(`BOO These arrays are NOT equal`)
+    }
+    else {
+        console.log(` YAY These arrays ARE equal`)
+    }
+}
+
+
+const takeUntil = function(array, callback) {
+  const arr = [];
+  for (let item of array){
+    let value = callback(item)
+    if (value){
+      return arr
+    }
+    arr.push(item)
+  }
+  return arr
+}
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
+
+// a function that is passed into another function as an argument - callback function
+
+
