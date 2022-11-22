b4255f590665f7a403a1011262013a59336c9986
AndrewGalatsan <agalatsan@hotmail.com>
2022-11-22 00:25:10 +0000

    Returns truthy callback from objects

---
+++ findKey.js
@@ -0,0 +1,43 @@
+const eqArrays = function(arr1,arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+
+const assertArraysEqual = function(arr1,arr2){
+  const result = eqArrays(arr1,arr2)
+    if (!result){
+        console.log(`BOO These arrays are NOT equal`)
+    }
+    else {
+        console.log(` YAY These arrays ARE equal`)
+    }
+}
+
+const findKey = function (object, callback) {
+    for(let key in object) {
+     if (callback(object[key])) {
+         return key;
+     } 
+  }
+  return undefined
+};
+
+console.log(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2))
+
+
+
