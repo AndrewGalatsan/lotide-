12bbad959aad425cb8acc26d0477cbd0509b7156
AndrewGalatsan <agalatsan@hotmail.com>
2022-11-25 16:47:33 +0000

    Detailed explanation of the head function and what the output should be of the test code.

--- head.js
+++ head.js
@@ -14,6 +14,9 @@ let head = function(arr){
   return arr[0]
 }
 
+// What the head function simply does is take in an array as a parameter and returns the first value of that array (index of 0) For the below test, it should
+// return passed because the index of [0] for both arrays are equal, and we are applying the assertEqual function to it as well.
+
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 
