b2ab533cd4cd414adaf441671111455694a7e23b
AndrewGalatsan <agalatsan@hotmail.com>
2022-11-25 16:32:22 +0000

    Detailed explanation of the findKeyByValue function and what the expected output should be for test cases.

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -32,6 +32,10 @@ const findKeyByValue = function(object,value){
   return undefined;
 };
 
+// What the findKeyByValue function does is take in 2 parameters; object and a value. Using a for in loop, we loop in through the index's of the object keys
+// and set a condition to see if the value is equal to the key, to console.log that value and return true, otherwise we return undefined. In the below test,
+// the first test should console.log the key and return true because drama is the key for 'The Wire"
+
 const bestTVShowsByGenre = { 
   sci_fi: "The Expanse",
   comedy: "Brooklyn Nine-Nine",
