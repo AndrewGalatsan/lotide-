ccf090365129abf27e70ec794ee85f70b6371eb2
AndrewGalatsan <agalatsan@hotmail.com>
2022-11-26 17:54:17 +0000

    Detailed explanation of the map function and callbacks functions, providing an output through an example as well.

--- map.js
+++ map.js
@@ -33,7 +33,10 @@ const map = function(array, callback) {
   return results;
 }
 
-
+// Here we are creating a function with two parameters, an array and a callback function. We start off by creating an empty array and then running a 
+// for of loop to go through the index's of the array. We then take the empty array we created and push the result of indexed array value where the callback function
+// is applied to it, and return the newly created array. In the example below the callback function specifies to identiy the first value inputted, 
+// represented as word[0]. When applying our map function the output should be g,c,t,m,t since that's the first values of the array in words.
 
 
 const results1 = map(words, word => word[0]);
