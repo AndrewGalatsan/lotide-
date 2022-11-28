853563e75f3a8a634b9581f51422945183461cad
AndrewGalatsan <agalatsan@hotmail.com>
2022-11-28 16:04:22 +0000

    Updated explanation of the without function.

--- without.js
+++ without.js
@@ -33,6 +33,11 @@ const without = function(source,itemsToRemove){
   return newArr;
 } 
 
+// The without function takes in a source as a parameter and also the itemsToRemove. We started off by creating a new array which contains all the elements
+// of the source. We run a for let loop through it and then run a for of loop through the itemsToRemove values. From here we see that if the source[i] is
+// is equal to the values of the for of loop, we create a new variable which is the index of thaat value. WE then take that array, splice the value that is
+// at the 'removeItem' index, and delete 1. We then return that new array.
+
 const words = ["hello", "world", "lighthouse"];
 without(words, ["lighthouse"]); 
 assertArraysEqual(words, ["hello", "world", "lighthouse"]);
