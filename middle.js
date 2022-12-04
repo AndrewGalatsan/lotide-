7c473b39cc217d9d05e24725b1109a4cff040289
AndrewGalatsan <agalatsan@hotmail.com>
2022-12-04 19:39:33 +0000

    Updated and refactored the middle function file and created a middle test file to apply the taught principles.

--- middle.js
+++ middle.js
@@ -1,25 +1,3 @@
-const eqArrays = function(arr1,arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function(arr1,arr2){
-  const result = eqArrays(arr1,arr2)
-    if (!result){
-        console.log(`BOO These arrays are NOT equal`)
-    }
-    else {
-        console.log(` YAY These arrays ARE equal`)
-    }
-}
-
 const middle = function(array) {
   let newArr = [];
     if (array.length <= 2 && array.length > 0){
@@ -33,6 +11,8 @@ const middle = function(array) {
     }
     return newArr;  
   }
+
+  module.exports = middle
 
   // The middle function takes in an array as a parameter and here we create a new empty array. For a condition, if the array length is less than or equal to 2
   // and the length is greater than 0, we console log a string saying that the array has 1 or 2 elements. ELse if the remainder of the length when divided by
---
+++ test/middleTest.js
@@ -0,0 +1,7 @@
+const middle = require('../middle');
+
+// Test code
+console.log(middle([1, 2, 3], [1, 2, 3]))
+console.log(middle([1, 2, 3], [3, 2, 1]))
+console.log(middle(["1", "2", "3"], ["1", "2", "3"])) 
+console.log(middle(["1", "2", "3"], ["1", "2", 3])) 
